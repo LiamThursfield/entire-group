9c2abcff964b7b2e0b168290bcfe47aedf47a0ad
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-10-13 22:47:45 +0100

    Add Jumbotron to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="container mx-auto px-3">
-    <h1 class="text-2xl">
-      Home Page
-    </h1>
+
+    <jumbotron
+      background_image="https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80"
+      background_overlay_rgba="30,30,30,0.75"
+    >
+      <h1 class="
+        font-black  pb-12 pt-32 px-12 text-4xl text-white
+        md:leading-tight md:text-6xl md:pb-16 md:pt-40 md:px-16
+      ">
+        Sample Header
+        <br>
+        Text Here
+      </h1>
+    </jumbotron>
+
+
   </div>
 </template>
 
 <script>
+  import Jumbotron from '~/components/containers/Jumbotron';
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      Jumbotron
+    }
   }
 </script>
