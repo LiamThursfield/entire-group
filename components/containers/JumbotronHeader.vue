14e9e77c3945463c29648bdbc1e5c5fdd37ddcb9
Liam Thursfield <liam.thursfield@nexuspoint.co.uk>
2019-10-28 20:34:05 +0000

    Add JumbotronHeader to avoid duplicating styles

---
+++ components/containers/JumbotronHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <jumbotron
+    :background_image="background_image"
+    :background_overlay_rgba="background_overlay_rgba"
+  >
+    <h1 class="
+      font-black px-12 py-16 text-center text-4xl text-white
+      md:leading-tight md:text-6xl md:pb-16 md:pt-40 md:px-16 md:text-left
+    ">
+      <slot></slot>
+    </h1>
+  </jumbotron>
+</template>
+<script>
+  import Jumbotron from '~/components/containers/Jumbotron';
+
+  export default {
+    name: "JumbotronHeader",
+    components: {
+      Jumbotron
+    },
+    props: {
+      background_image: {
+        default: '',
+        type: String
+      },
+      background_overlay_rgba: {
+        default: '',
+        type: String
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="container mx-auto px-3">
 
-    <jumbotron
+    <jumbotron-header
       background_image="https://images.unsplash.com/photo-1504328345606-18bbc8c9d7d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80"
       background_overlay_rgba="30,30,30,0.75"
     >
-      <h1 class="
-        font-black px-12 py-16 text-center text-4xl text-white
-        md:leading-tight md:text-6xl md:pb-16 md:pt-40 md:px-16 md:text-left
-      ">
         Sample Header
-        <br class="
-          hidden
-          md:visible
-        ">
+        <br class="hidden md:inline">
         Text Here
-      </h1>
-    </jumbotron>
+    </jumbotron-header>
 
 
   </div>
 </template>
 
 <script>
-  import Jumbotron from '~/components/containers/Jumbotron';
+  import JumbotronHeader from '~/components/containers/JumbotronHeader';
+
   export default {
     name: 'Home',
     components: {
-      Jumbotron
+      JumbotronHeader
     }
   }
 </script>
